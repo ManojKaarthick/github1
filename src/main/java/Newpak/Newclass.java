5b12a5043cefb8d96f7431d61fe9430cdb1b2aec
ManojKaarthick <manojkaarthick2@gmail.com>
2024-05-20 10:04:16 +0530

    initial commit

---
+++ src/main/java/Newpak/Newclass.java
@@ -0,0 +1,22 @@
+package Newpak;
+
+public class Newclass {
+
+    public void m3(){
+
+        System.out.println("hello gd mor");
+    }
+
+    public void m1(){
+        System.out.println("hello gd eve");
+
+    }
+    public static void main(String[] args) {
+        Newclass n=new Newclass();
+        n.m3();
+        n.m1();
+
+
+
+    }
+}
---
+++ src/test/java/normalclass/Newclass2.java
@@ -0,0 +1,29 @@
+package normalclass;
+
+
+import Newpak.Newclass;
+
+public class Newclass2 {
+
+    public void manoj(){
+        System.out.println("hiiiiiii");
+    }
+
+    public void kummar(){
+        System.out.println("helooooooo");
+
+
+    }
+
+    public static void main(String[] args) {
+        Newclass2 ns=new Newclass2();
+        ns.kummar();
+    }
+
+
+     }
+
+
+
+
+
